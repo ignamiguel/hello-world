e4783bf08f909fa6334ad85ae1e20ac79423d124
ignamiguel <iglesiasnacho@gmail.com>
2015-03-31 15:44:55 -0300

    Aplicacion terminada

--- chatrooms/public/javascript/chat_ui.js
+++ chatrooms/public/javascript/chat_ui.js
@@ -22,6 +22,7 @@ function processUserInput(chatApp, socket) {
 }
 
 var socket = io.connect();
+
 $(document).ready(function () {
 	var chatApp = new Chat(socket);
 	socket.on('nameResult', function (result) {
